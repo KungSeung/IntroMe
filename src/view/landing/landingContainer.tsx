4c52d0059f8747d69743bde5dc305c0a488b7582
KungSeung <nkhone123456@naver.com>
2023-10-24 21:49:12 +0900

    landingPage

---
+++ src/view/landing/landingContainer.tsx
@@ -0,0 +1,3 @@
+function landingContainer() {}
+
+export default landingContainer;
---
+++ src/view/landing/landingView.tsx
@@ -0,0 +1,14 @@
+function landingView() {
+  return (
+    <div className="container">
+      <nav className="global-nav">
+        <img src="img/reCircle.png" alt="redCircle" className="LogoImage" />
+        <a href="#" className="global-nav-item">
+          나에 대하여
+        </a>
+      </nav>
+    </div>
+  );
+}
+
+export default landingView;
